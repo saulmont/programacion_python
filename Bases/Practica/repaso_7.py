9644b8f1b22e223a363fdbd8ed42bed64aa2b2f0
Saúl Montalvo <saul.info@gmail.com>
2020-11-26 17:19:05 -0500

    Hice una practica de una funcion para generar la serie de fibonacci.

---
+++ Bases/Practica/repaso_7.py
@@ -0,0 +1,27 @@
+def fibonaci(fib):
+    a = 0
+    b = 1
+
+    for i in range (fib):
+        c = a + b
+        a = b
+        b = c
+
+    return b
+
+#print(fibonaci(5))
+
+#con un while
+a = 0
+b = 1
+
+c = 0
+d = 5
+
+while c < d:
+    e = a + b
+    a = b
+    b = e
+    print(e)
+
+    c = c + 1
